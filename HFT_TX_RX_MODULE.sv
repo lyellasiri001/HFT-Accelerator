8a350c8791fcae7f7cf50a3149884acf43b0fdfc
lyellasiri001 <91278036+lyellasiri001@users.noreply.github.com>
2025-05-21 18:34:29 -0700

    added double flip-flop and additional registers for FSM

--- HFT_TX_RX_MODULE.sv
+++ HFT_TX_RX_MODULE.sv
@@ -1,43 +1,97 @@
-module tx_rx(clk, rst,rx,rx_clear,rx_ready,rx_data);
-	input logic clk, rst, rx,rx_clear;
-	output logic rx_ready;
+module uart_rx(clk, rst,rx,rx_ready,rx_data);
+	input logic clk, rst, rx;
+	output logic rx_avail;
 	output logic rx_data[7:0];
+	
+	logic rx_buffer; 
+	logic [15:0] clk_count;
+	logic [2:0] bit_index; 
 	
 	parameter baud_rate = 115200;
 	parameter clk_frq_hz = 10000000;
 	parameter clks_per_bit = clk_frq_hz / baud_rate;
 	
-	enum {IDLE,START,DATA,STOP,DONE} ps, ns;
+	enum logic [2:0] {IDLE,START,DATA,STOP} ps, ns;
 	
-	always_comb begin
-		ns = ps;
-		rx_done = 0;
+	always_comb 
+		begin 
+			
+			case(ps) 
+			
+			IDLE: 
+				begin 
+					bit_index = b'00;
+				end
+			
+			START: 
+				begin 
+					bit_index = b'01;
+				end 
+			
+			DATA: 
+				begin 
+					bit_index = b'10;
+				end
+			
+			STOP:
+				begin 
+					bit_index = b'11;
+				end
+		endcase 	
+			
+			
+	
+	always_comb 
+		begin
+			ns = ps;
 		
-		case(ps)
-			
-			IDLE : 	if(~rx) ns = START;
-					else if ((rx & DONE) | ~START) ns = IDLE;
-			
-		
-			START : 	if (clk_count = clks_per_bit / 2)
-							if (~rx) ns = START;
-						else ns = IDLE;
-			
-			DATA : if (clk_count = clks_per_bit) 
-							if (bit_index = b'07)
-								ns = STOP;
-					 else ns = DATA;
+			case(ps)
+				
+				IDLE:
+					begin
+						if(~rx) ns = START;
+							else ns = IDLE;
+					end
 				
 			
-			STOP : if (clk_count = clks_per_bit) ns = STOP;
-			
-			DONE : if (STOP) ns = IDLE; 
-			endcase
-		end	
+				START :
+					begin
+						if (clk_count = (clks_per_bit-1) / 2)
+									if (~rx) ns = START;
+								else ns = IDLE;
+					end
+				
+				DATA : 
+					begin
+						if (clk_count = clks_per_bit-1) 
+							if (bit_index = b'07) ns = STOP;
+								else ns = DATA;
+					end
+					
+				
+				STOP : 
+					begin
+						;
+					end 
+					
+		endcase
+		
+	always_ff @(posedge clk) begin 
+	 rx_buffer <= rx;
+	 rx <= buffer;
+	 end 
 	
 	always_ff @(posedge clk) begin
-		if (rst) 
-		ns <= IDLE 
+		if (rst) begin 
+			counter <= 0;
+			bit_index <= 0;
+		if (ps <= DATA) clk <= clk + 1;
+	
+		ps <= ns;
+			end 
+		
+		 
+		
 		
 		
 		
